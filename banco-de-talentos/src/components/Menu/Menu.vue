c99ff14d8f66d734e8c0a4f0750775c713141691
lcsrlm <lucas_rosa-lima@estudante.sesisenai.org.br>
2023-08-14 18:22:25 -0300

    melhoria visual na navbar

--- banco-de-talentos/src/components/Menu/Menu.vue
+++ banco-de-talentos/src/components/Menu/Menu.vue
@@ -1,26 +1,28 @@
 <template>
-    <h1 class="title">Banco de talentos</h1>
-        <ul class="navbar">
-            <RouterLink to="/">
-                <li class="item-nav">
-                <img width="24" height="24" src="https://img.icons8.com/material-sharp/24/home.png" alt="home"/>
-                <h3>Home</h3>
-            </li>
-            </RouterLink>
-            <RouterLink to="/about">
-                <li class="item-nav">
-                <img width="24" height="24" src="https://img.icons8.com/material-sharp/24/info.png" alt="info"/>
-                <h3>Sobre</h3>
-            </li>                
-            </RouterLink>
-            <RouterLink to="/talent/new">
-                <li class="item-nav">
-                    <img width="24" height="24" src="https://img.icons8.com/material-sharp/24/000000/task.png" alt="task"/>
-                <h3>Cadastro</h3>
-            </li>
-            </RouterLink>
-        </ul>
-</template>
+    <div class="navbar-container">
+      <h1 class="title">Banco de talentos</h1>
+      <ul class="navbar">
+        <RouterLink to="/" class="nav-link">
+          <li class="item-nav">
+            <img width="24" height="24" src="https://img.icons8.com/material-sharp/24/home.png" alt="home" />
+            <span>Home</span>
+          </li>
+        </RouterLink>
+        <RouterLink to="/about" class="nav-link">
+          <li class="item-nav">
+            <img width="24" height="24" src="https://img.icons8.com/material-sharp/24/info.png" alt="info" />
+            <span>Sobre</span>
+          </li>
+        </RouterLink>
+        <RouterLink to="/talent/new" class="nav-link">
+          <li class="item-nav">
+            <img width="24" height="24" src="https://img.icons8.com/material-sharp/24/000000/task.png" alt="task" />
+            <span>Cadastro</span>
+          </li>
+        </RouterLink>
+      </ul>
+    </div>
+  </template>
 
 <style>
 :root {
@@ -56,5 +58,38 @@
     background-color: #f9f9f9;
     border-radius: 10px;
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.navbar-container {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.navbar {
+  display: flex;
+  justify-content: center;
+  list-style: none;
+  gap: 20px;
+}
+
+.nav-link {
+  text-decoration: none;
+  color: inherit;
+}
+
+.item-nav {
+  display: flex;
+  padding: 8px;
+  gap: 8px;
+  align-items: center;
+  background-color: #f9f9f9;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  transition: background-color 0.3s ease, transform 0.2s ease;
+}
+
+.item-nav:hover {
+  background-color: rgb(200, 200, 200);
+  transform: scale(1.02);
 }
 </style>
